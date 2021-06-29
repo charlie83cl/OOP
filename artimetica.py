36bb36a20fee86ab29ded204ef26823845d92fd1
charlie83cl <82404370+charlie83cl@users.noreply.github.com>
2021-06-29 09:31:23 -0400

    dia 29 de Junio 2021 agregando tarea de modulos asistida por el Profesor

---
+++ artimetica.py
@@ -0,0 +1,19 @@
+import math
+
+def suma(x, y):
+    return x + y
+
+def multiplica(x, y):
+    return x * y
+
+def resta(x, y):
+    return x - y
+
+def divide(x, y):
+    if y == 0:
+        return "error"
+    return x/ y
+    
+def areCircu(r):
+    return  math.pi * r**2
+
---
+++ calculos.py
@@ -0,0 +1,11 @@
+import artimetica
+print(artimetica.suma(5, 8))
+print(artimetica.resta(10, 5))
+print(artimetica.multiplica(12, 6))
+print(artimetica.divide(12, 6))
+print(artimetica.divide(100, 0))
+print(artimetica.divide(1, 1000))
+
+print(artimetica.areCircu(12))
+print(artimetica.areCircu(100))
+
